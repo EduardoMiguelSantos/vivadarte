c8c24c6e26f974063a0c9a63597efe60c135c382
Eduardo <eduardosantos21092004@gmail.com>
2026-05-01 01:58:23 +0100

    frontend atualizado

--- backend/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { login, pedidoRegisto } = require('../controllers/autController');
-
-router.post('/login', login);
-router.post('/pedido-registo', pedidoRegisto);
-
-module.exports = router;
