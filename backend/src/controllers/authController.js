1167f8c0deb5e80cbb41268ce888c93261cc5020
Eduardo <eduardosantos21092004@gmail.com>
2026-05-02 01:58:57 +0100

    save   update

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const userModel = require('../models/userModel');
+const userModel = require('../models/utilizadorModel');
 const { pool } = require('../config/db');
 
 const PERFIL_MAP = {
